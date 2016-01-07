024065bf5a81c683181c9068ce106491ede22dcb
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-01-07 21:27:00 +0100

    Initial commit

--- index.js
+++ index.js
@@ -4,25 +4,41 @@ var fs = require('fs')
 var FormData = require('form-data')
 var data = require('./test/data/data.json')
 var template = fs.createReadStream('test/data/template.docx')
-var file = fs.createWriteStream('test/data/document.docx')
-var url = 'https://templater.service.t-fk.no'
-var form = new FormData()
+var file = fs.createWriteStream('test/data/document.pdf')
+// var templateServiceUrl = 'https://templater.service.t-fk.no'
+// var pdfServiceUrl = 'https://pdfconvert.service.t-fk.no'
+var templateServiceUrl = 'http://192.168.99.100'
+var pdfServiceUrl = 'http://192.168.99.100:3000'
+var templateForm = new FormData()
+var pdfForm = new FormData()
+var timeStart = new Date().getTime()
 
 Object.keys(data).forEach(function (key) {
-  form.append(key, data[key])
+  templateForm.append(key, data[key])
 })
 
-form.append('file', template)
+templateForm.append('file', template)
 
-form.submit(url, function (error, response) {
+templateForm.submit(templateServiceUrl, function (error, response) {
   if (error) {
     throw error
   } else {
     console.log(response.statusCode)
     console.log(response.statusMessage)
-    response.pipe(file)
-    file.on('finish', function () {
-      console.log('written')
+    pdfForm.append('file', response)
+    pdfForm.submit(pdfServiceUrl, function (err, resp) {
+      if (err) {
+        throw err
+      } else {
+        console.log(resp.statusCode)
+        console.log(resp.statusMessage)
+        resp.pipe(file)
+        file.on('finish', function () {
+          var timeStop = new Date().getTime()
+          console.log('written')
+          console.log(timeStop - timeStart)
+        })
+      }
     })
   }
 })
